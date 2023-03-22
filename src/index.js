773c9b662c7049f4592ae771f1780dc8ed42132c
Ivan Kornienko <ivan@homeds.pro>
2023-03-22 23:34:44 +0200

    add class

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
 // import
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  console.log(e.target.value);
+}
 
 fetch(
-  'https://restcountries.com/v3.1/name/sw?fields=name,capital,population,flags,languages'
+  'https://restcountries.com/v3.1/name/deu?fields=name,capital,population,flags,languages'
 )
   .then(r => r.json())
   .then(data => {
--- src/js/serviceAPI.js
+++ src/js/serviceAPI.js
@@ -1,3 +1,32 @@
-fetch('https://restcountries.com/v3.1/name/deutschland')
-  .then(r => r)
-  .then(console.log);
+const BASE_URL = 'https://restcountries.com/v3.1/';
+
+class countryAPI {
+  constructor() {
+    this.responce = '';
+    this.data = {};
+    this.amount = 0;
+    this.dataArray = [];
+  }
+  fetchFromServer() {
+    fetch(`${BASE_URL} name / ${responce}`)
+      .then(r => r.json())
+      .then(data => {
+        if (data.length > 1) {
+          this.dataArray = data.map(element => {
+            ({
+              flags: data.flags.svg,
+              name: data.name.official,
+            });
+          });
+        } else {
+          this.data = {
+            flags: data.flags.svg,
+            name: data.name.official,
+            capital: data.capital[0],
+            population: data.population,
+            languages: data.languages,
+          };
+        }
+      });
+  }
+}
