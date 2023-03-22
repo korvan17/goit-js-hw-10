2ea332904ddbec2b206964ba5611c15018178a6c
Ivan Kornienko <ivan@homeds.pro>
2023-03-22 22:42:27 +0200

    begining

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+// import
 const DEBOUNCE_DELAY = 300;
+
+fetch(
+  'https://restcountries.com/v3.1/name/sw?fields=name,capital,population,flags,languages'
+)
+  .then(r => r.json())
+  .then(data => {
+    console.log(data);
+    Notify.success(`data.length - ${data.length}`);
+  });
---
+++ src/js/serviceAPI.js
@@ -0,0 +1,3 @@
+fetch('https://restcountries.com/v3.1/name/deutschland')
+  .then(r => r)
+  .then(console.log);
