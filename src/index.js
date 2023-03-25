5f6ed07f631bf54bb6d868d498e696453796df2a
Ivan Kornienko <ivan@homeds.pro>
2023-03-25 19:57:07 +0200

    end of task

--- src/index.js
+++ src/index.js
@@ -1,23 +1,57 @@
 import './css/styles.css';
+import APIservices from './js/serviceAPI';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
-// import
+import manyMarkup from './templates/manyCountry.hbs';
+import oneMarkup from './templates/oneCcountry.hbs';
+
+// console.log(manyMarkup({ flags: 'first', name: 'name' }));
+
+const apiCountry = new APIservices();
 const DEBOUNCE_DELAY = 300;
 const refs = {
   input: document.querySelector('#search-box'),
+  text: document.querySelector('.country-list'),
 };
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
+  //   refs.text.innerHTML = '';
   console.log(e.target.value);
+  apiCountry.responce = e.target.value.trim();
+  if (apiCountry.responce) {
+    apiCountry.fetchFromServer().then(markUp);
+  } else {
+    refs.text.innerHTML = '';
+  }
 }
 
-fetch(
-  'https://restcountries.com/v3.1/name/deu?fields=name,capital,population,flags,languages'
-)
-  .then(r => r.json())
-  .then(data => {
-    console.log(data);
-    Notify.success(`data.length - ${data.length}`);
-  });
+function markUp(data) {
+  if (data.length > 10) {
+    refs.text.innerHTML = '';
+    Notify.success(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (data.length === 1) {
+    refs.text.innerHTML = '';
+    refs.text.insertAdjacentHTML(
+      'beforeend',
+      oneMarkup(oneCountryMarkup(data[0]))
+    );
+  } else {
+    refs.text.innerHTML = '';
+    refs.text.insertAdjacentHTML('beforeend', manyMarkup(data));
+  }
+}
+
+function oneCountryMarkup({ flags, name, capital, languages, population }) {
+  const data = {
+    flags,
+    name,
+    population,
+    capital,
+  };
+  data.languages = Object.values(languages).join(', ');
+  return data;
+}
--- src/js/serviceAPI.js
+++ src/js/serviceAPI.js
@@ -1,32 +1,26 @@
-const BASE_URL = 'https://restcountries.com/v3.1/';
+import { Notify } from 'notiflix';
+const BASE_URL = 'https://restcountries.com/v3.1';
 
-class countryAPI {
+export default class countryAPI {
   constructor() {
     this.responce = '';
     this.data = {};
     this.amount = 0;
     this.dataArray = [];
   }
+
   fetchFromServer() {
-    fetch(`${BASE_URL} name / ${responce}`)
-      .then(r => r.json())
-      .then(data => {
-        if (data.length > 1) {
-          this.dataArray = data.map(element => {
-            ({
-              flags: data.flags.svg,
-              name: data.name.official,
-            });
-          });
-        } else {
-          this.data = {
-            flags: data.flags.svg,
-            name: data.name.official,
-            capital: data.capital[0],
-            population: data.population,
-            languages: data.languages,
-          };
+    return fetch(
+      `${BASE_URL}/name/${this.responce}?fields=name,capital,population,flags,languages`
+    )
+      .then(response => {
+        if (!response.ok) {
+          Notify.failure('Oops, there is no country with that name');
+          throw new Error(response.status);
         }
-      });
+
+        return response.json();
+      })
+      .then(data => data);
   }
 }
